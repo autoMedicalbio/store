54f4ac54988c674a15c6afb56b2ed96583f5c7ce
chenli7 <chenliw@yonyou.com>
2019-07-13 23:33:11 +0800

    index

--- js/index.js
+++ js/index.js
@@ -1,22 +1,38 @@
 $(function () {
-    var template = "<label>姓名：</label>\n" +
-        "            <input type=\"text\" name=\"fullname\" >\n" +
+    var template = "<div class='info'>\n" +
+        "    <p>被检测人信息</p>\n" +
+        "    <label>姓名：</label>\n" +
+        "    <input type='text' name='fullname' class='name'>\n" +
         "\n" +
-        "            <label>性别：</label>\n" +
-        "            <input type=\"text\" name=\"fullname\" >\n" +
+        "    <label>性别：</label>\n" +
+        "    <select name='sex' class='sex'>\n" +
+        "        <option value='#'>请选择</option>\n" +
+        "        <option value='0'>男</option>\n" +
+        "        <option value='1'>女</option>\n" +
+        "    </select>\n" +
         "\n" +
-        "            <label>是否有身份证：</label>\n" +
-        "            <select name=\"idCard\">\n" +
-        "                <option value=\"yes\">是</option>\n" +
-        "                <option value=\"no\">否</option>\n" +
-        "            </select>\n" +
+        "    <label><span class='red'>&nbsp;*&nbsp;</span>是否有身份证：</label>\n" +
+        "    <select name='idCard' class='idCard'>\n" +
+        "        <option value='#'>请选择</option>\n" +
+        "        <option value='yes'>是</option>\n" +
+        "        <option value='no'>否</option>\n" +
+        "    </select>\n" +
         "\n" +
-        "            <label>样本信息：</label>\n" +
-        "            <select name=\"info\">\n" +
-        "                <option value=\"kqlm\">口腔黏膜</option>\n" +
-        "                <option value=\"qdx\">脐带血</option>\n" +
-        "            </select>"
-    var price  , name ;
+        "    <label><span class='red'>&nbsp;*&nbsp;</span>样本信息：</label>\n" +
+        "    <select name='info' class='info'>\n" +
+        "        <option value='#'>请选择</option>\n" +
+        "        <option value='kqlm'>口腔黏膜</option>\n" +
+        "        <option value='qdx'>脐带血</option>\n" +
+        "    </select>\n" +
+        "    <label class='label-textarea'>\n" +
+        "        <span class='red'>&nbsp;*&nbsp;</span>\n" +
+        "        邮寄地址：\n" +
+        "        <span class='right'><input type='checkbox' class='addressInput'/>自定义邮寄地址</span>\n" +
+        "    </label>\n" +
+        "    <textarea type='text' name='textarea' class='address'></textarea>\n" +
+        "   <a class='delInfo'>删除</a>"
+        "</div>"
+    var price  , name , productList =[] ;
     //良药套餐列表
     $(".menu").on( "click", (function (e) {
         var total =Number($(".meal-price").html() ) ;
@@ -24,12 +40,18 @@ $(function () {
         price = Number( el.val() );
         name =  el.attr("data-name");
         if(el.is(":checked")){
+            productList.push({price,name})
            total += price;
         }else{
+            _.remove(productList,function (val) {
+                return val.name == name ;
+            })
            total -= price;
         }
+
         total = total >0 ? total :0 ;
         $(".meal-price").html(total);
+        isCheckProduct();
     }))
     $(".next-btn").click(function () {
         if(!price ||  price == 0 ){
@@ -38,15 +60,29 @@ $(function () {
             window.location.href ="index.html"
     })
     $("#addUserInfo").click(function () {
-        $("#info2").append(template);
-        $("#info2").trigger("create")
+        $("#userInfo").append(template);
+        $("#userInfo").trigger("create")
     })
-    //只读
-    $("#b").prop("readonly",true)
 
-    name = name ? name : localStorage.getItem("name")
-    price = price ? price : localStorage.getItem("price")
-    $("#tc-price").html("已选套餐："+ name + "价格:" + price)
+    function isCheckProduct() {
+        var html = "";
+        _.each(productList,function (val) {
+            html += "<p><span>" + val.name  + "</span>" + "：" + "<span>" + val.price  + "</span></p>"
+        })
+
+        $("#tc-price").html(html);
+    }
+
+
+    $("body").on("click", ".delInfo",delInfo );
+    function delInfo(e){
+        var el = $(e.currentTarget);
+        el;
+        el.parent(".info").remove();
+    }
+
+
+
 
 
 });
